b8744c97977811417fa631550fb6886a8d5639f1
Sergio Alexander Florez Galeano <xergioalex@gmail.com>
2021-06-09 12:03:28 +0000

    Fix nuxt issue

---
+++ pages/auth/connect-slack.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="container">
+    <div class="content">
+      <a href="/">
+        <img class="logo" src="/images/logo.png" alt="LaraBot Logo" />
+      </a>
+      <h2>ChatBot Assistant {{ data() }}</h2>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  methods: {
+    data() {
+      return 'Xergio AleX'
+    },
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.links {
+  padding-top: 15px;
+}
+
+.content {
+  padding: 0 20px;
+  .logo {
+    width: 300px;
+  }
+  h2 {
+    margin-top: 15px;
+    margin-bottom: 15px;
+  }
+  .google-play-logo {
+    width: 200px;
+  }
+}
+// https://app.dailybot.com/auth/connect-slack?code=3251795295.2146125224998.ed60e856731caca0f24e93e3ba20df7915cde9952caa644a5383adcf3a01f1de&state=rly7ss05oj
+</style>
